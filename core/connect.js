6880ad0bfe19562b331b9b2763ed372d147c7878
pongsathorn <60050208@kmitl.ac.th>
2021-07-08 08:16:04 +0700

    add new connect pool

--- core/connect.js
+++ core/connect.js
@@ -1,15 +1,30 @@
 const mysql = require('mysql2');
 
+// const pool = mysql.createPool({
+//     connectionLimit : 10,
+//     host     : 'test.cedtywdc51j1.us-east-2.rds.amazonaws.com',
+//     // host     : '127.0.0.1',
+//     user     : 'admin',
+//     password : '11223344',
+//     database : 'final-project',
+//     port     : '3306',
+//     debug    : false,
+//     multipleStatements: true
+// });      
+
 const pool = mysql.createPool({
-    connectionLimit : 10,
-    host     : 'test.cedtywdc51j1.us-east-2.rds.amazonaws.com',
-    user     : 'admin',
-    password : '11223344',
-    database : 'final-project',
-    port     : '3306',
-    debug    : false,
-    multipleStatements: true
-});      
+  connectionLimit : 10,
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : '',
+  database : 'final-project',
+  port     : '3306',
+  debug    : false,
+  multipleStatements: true
+});
+
+
+
 
 const promiseQuery = (sql, prepared_arr = []) => {
     return new Promise((resolve,reject) => {
